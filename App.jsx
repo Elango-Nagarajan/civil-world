a95becc7c1ba5b9628752a28520fdc1a7ea3755e
Elango-Nagarajan <130850649+Elango-Nagarajan@users.noreply.github.com>
2025-06-21 16:14:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif", textAlign: "center" }}>
+      <h1>🚧 Welcome to Civil World</h1>
+      <p>This is a Vercel-ready version of your app.</p>
+    </div>
+  );
+}
+
+export default App;
